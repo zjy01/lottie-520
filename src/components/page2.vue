c702a4758ff3e7946bb99514d5672a1a3f6b9021
carvenzhang <carvenzhang@tencent.com>
2024-05-16 17:23:36 +0800

    feat: 对联动画完成

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    <div class="left">
+    <div class="left load" v-if="leftAnimation">
       <img src="../assets/zjh-flash.png" />
       <div class="dl">
         <div class="text">
@@ -14,7 +14,7 @@
         </div>
       </div>
     </div>
-    <div class="right">
+    <div class="right load" v-if="rightAnimation">
       <img src="../assets/zzb-flash.png" />
       <div class="dl">
         <div class="text">
@@ -28,19 +28,40 @@
         </div>
       </div>
     </div>
-    <div class="top">
+    <div class="top load" v-if="topAnimation">
       <div class="dl dl-t">
-        <span>善</span>
-        <span>心</span>
-        <span>美</span>
-        <span>人</span>
+        <div class="text">
+          <span>善</span>
+          <span>心</span>
+          <span>美</span>
+          <span>人</span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 
+const dlDuration = 5000;
+const rightAnimation = ref(false);
+const leftAnimation = ref(false);
+const topAnimation = ref(false);
+onMounted(() => {
+  // 上联加载
+  setTimeout(() => {
+    rightAnimation.value = true;
+  });
+  // 下联加载
+  setTimeout(() => {
+    leftAnimation.value = true;
+  }, dlDuration);
+  // 横幅加载
+  setTimeout(() => {
+    topAnimation.value = true;
+  }, 0);
+})
 </script>
 
 <style scoped lang="less">
@@ -61,6 +82,9 @@
     left: -4vw;
     top: 2vw;
   }
+  &.load img {
+    animation: fadeInLeft 1s;
+  }
 }
 .right { 
   position: absolute;
@@ -73,19 +97,69 @@
     right: -5vw;
   }
 }
+.load {
+  img {
+    animation: fadeInRight 1s;
+  }
+  .dl {
+    @t: 5s;
+    height: 0vw;
+    animation: dl-v-animation @t linear 1s forwards;
+    span {
+      each(range(7), {
+        &:nth-child(@{value}) {
+          animation: jackInTheBox 1s ease-in-out (@t / 9 * (@value + 1) + 0.5s) forwards;
+        }
+      });
+    }
+  }
+  .dl-t {
+    @t: 2s;
+    width: 0vw;
+    animation: dl-h-animation @t linear;
+    span {
+      each(range(4), {
+        &:nth-child(@{value}) {
+          animation: jackInTheBox 1s ease-in-out (@t / 5 * (4 - @value + 1)) forwards;
+        }
+      });
+      // &:nth-child(4) {
+      //   animation: pulse 1s ease-in-out forwards;
+      // }
+      // &:nth-child(3) {
+      //   animation: pulse 1s ease-in-out 1+.8s forwards;
+      // }
+      // &:nth-child(2) {
+      //   animation: pulse 1s ease-in-out 1+.8s * 2 forwards;
+      // }
+      // &:nth-child(1) {
+      //   animation: pulse 1s ease-in-out 1+.8s * 3 forwards;
+      // }
+    }
+  }
+}
 .top { 
   position: absolute;
-  height: 15vw;
-  width: 50vw;
   top: 5vw;
   left: 25vw;
+  height: 15vw;
+  width: 50vw;
   .dl-t {
     top: 0;
-    width: 100%;
-    height: 100%;
+    height: 15vw;
+    width: 50vw;
     line-height: 15vw;
     font-size: 9vw;
     background-image: url('../assets/dl-h.png');
+    background-size: 50vw 100%;
+    background-position: right; 
+    .text {
+      padding-top: 0;
+      height: 15vw;
+      width: 50vw;
+      left: unset;
+      right: 0;
+    }
   }
 }
 .dl {
@@ -99,11 +173,15 @@
   line-height: 1.4em;
   color: #e4dbab;
   background-image: url('../assets/dl-s.png');
-  background-size: 100% 100%; 
+  background-size: 100% 128vw; 
   background-repeat: no-repeat;
-  img {
-    width: 100%;
+  overflow: hidden;
+
+  span {
+    display: inline-block;
+    opacity: 0;
   }
+
   .text {
     position: absolute;
     padding-top: 15vw;
@@ -113,4 +191,23 @@
     height: 100%;
   }
 }
+
+@keyframes dl-v-animation {
+  from {
+    height: 0vw;
+  }
+  to {
+    height: 128vw;
+  }
+}
+
+@keyframes dl-h-animation {
+  from {
+    width: 0;
+  }
+  to {
+    width: 50vw;
+  }
+}
+
 </style>
